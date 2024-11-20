2807addf589fe9e0e1e2e13d7f20835b83aafb7a
taeaeaexin <re_fuse@naver.com>
2024-11-20 21:24:49 +0900

    241120

--- src/main/java/com/example/todo_app/TodoAppApplication.java
+++ src/main/java/com/example/todo_app/TodoAppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoAppApplication.class, args);
+		SpringApplication.
+				run(TodoAppApplication.class, args);
 	}
 
 }
